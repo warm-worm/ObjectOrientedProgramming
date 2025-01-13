d30025f37b790a96c015f62bde6e7fed4189a4f7
Aleksandra Korniak <ola.korniak@gmail.com>
2025-01-13 16:31:51 +0100

    Add files via upload

---
+++ Class with Multiple Components/4/tv.py
@@ -0,0 +1,30 @@
+# tv.py file
+# class definition
+class TV:
+    def __init__(self):
+        self.is_on = False
+        self.channel_no = 1
+        self.channel_names = []
+    def turn_off(self):
+        self.is_on = False
+    def turn_on(self):
+        self.is_on = True
+    def set_channels(self, channels_list):
+        self.channel_names = channels_list
+    def show_channels(self):
+        if self.channel_names:
+            print('Channel list:')
+            for index, channel in enumerate(self.channel_names, 1):
+                print(f'{index}. {channel}')
+    def set_channel(self, new_channel_no):
+        if 1<= new_channel_no <= len(self.channel_names):
+            self.channel_no = new_channel_no
+        else:
+            print('Invalid channel number.')
+    def show_status(self):
+        if self.is_on:
+            print(f'The TV is on, the channel number is: {self.channel_no}')
+        else:
+            print('The TV is off')
+
+
--- Class with Multiple Components/4/tv_show.py
+++ Class with Multiple Components/4/tv_show.py
@@ -1,1 +1,34 @@
+# tv_show.py file
+# main program
+import tv
 
+def main():
+    # object creation
+    my_tv = tv.TV()
+
+    # object usage
+    print('Create TV set')
+    my_tv.show_status() #current status of the tv
+
+    print('Turn TV on')
+    my_tv.turn_on()
+    my_tv.show_status()
+
+    print('Display the list of available channels')
+    my_tv.show_channels()
+
+    print('Set TV channels: TVP1, TVP2, Polsat, TVN, Filmbox, Discovery')
+    channels_list = ["TVP1", "TVP2", "Polsat", "TVN", "Filmbox", "Discovery"]
+    my_tv.set_channels(channels_list)
+    my_tv.show_channels()
+    
+    my_tv.show_status()
+
+    print('Turn TV off')
+    my_tv.turn_off()
+    my_tv.show_status()
+
+if __name__ == "__main__":
+   main() 
+
+# channel_names = ["TVP1", "TVP2", "Polsat", "TVN", "Filmbox", "Discovery"]
