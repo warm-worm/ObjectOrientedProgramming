8e7b59857c885131deae9d5b653831b36271dffc
Aleksandra Korniak <ola.korniak@gmail.com>
2025-01-13 15:52:31 +0100

    Add files via upload

--- Class with Multiple Components/1/tv.py
+++ Class with Multiple Components/1/tv.py
@@ -1,1 +1,14 @@
-
+# tv.py file
+# class definition
+class TV:
+    def __init__(self):
+        self.is_on = False
+    def turn_off(self):
+        self.is_on = False
+    def turn_on(self):
+        self.is_on = True
+    def show_status(self):
+        if self.is_on:
+            print('The TV is on')
+        else:
+            print('The TV is off')
